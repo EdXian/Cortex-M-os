c6103cf4c7cbf2cd0af420965c5c4c9d94b01b89
EdXian <f29993856@gmail.com>
2019-11-17 21:35:55 +0800

    add periodic_os

---
+++ perodic_os/Objects/led_nucleo_f401re.d
@@ -0,0 +1,11 @@
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\Keil\STM32NUCLEO_BSP\1.7.0\Boards\ST\NUCLEO-F401RE\Common\LED_NUCLEO_F401RE.c
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f401xe.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
+.\objects\led_nucleo_f401re.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.9.0\Board\Board_LED.h
---
+++ perodic_os/kernel/cs.s
@@ -0,0 +1,57 @@
+	AREA |.text|,CODE,READONLY,ALIGN=2
+	THUMB
+	PRESERVE8		
+	EXTERN current_tcb
+	EXPORT PendSV_Handler
+	EXPORT osscehdulerlaunch
+	EXTERN	system_tick		
+	EXTERN	osschedular
+
+PendSV_Handler	;save r0 r1 r2 r3 r12 lr pc psr	automatically
+	CPSID		I
+	PUSH		{R4-R11}
+	LDR			R0,=current_tcb
+	LDR			R1,[R0]
+	STR			sp,[R1]
+	
+	PUSH		{R0,LR}
+	BL			osschedular
+	POP			{R0,LR}
+	LDR			R1,[R0]
+	
+	;LDR			R1,[R1,#4]  
+	;STR			R1,[R0]
+	
+	LDR			sp,[R1]
+	
+	POP			{R4-R11}
+	CPSIE		I
+	BX			LR
+	
+	
+;	LDR			R0,=system_tick
+;	LDR			R1,[R0]
+;	ADD			R1,R1,#1
+;	STR			R1,[R0]
+	
+
+
+osscehdulerlaunch
+	LDR		R0,=current_tcb
+	LDR		R2,[R0]
+	
+	LDR		sp,[R2]   ;
+	
+	POP		{R4-R11}
+	POP		{R0-R3}
+	POP		{R12}
+	ADD		sp,sp,#4
+	POP		{LR}
+	ADD		sp,sp,#4
+	CPSIE	I
+	BX		LR
+	ALIGN
+	END
+	
+
+
